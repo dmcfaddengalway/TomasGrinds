3573b64857b5a37bd53ac3a7a6b655df17f4a0a4
Daniel McFadden <dmcfaddengalway@gmail.com>
2020-09-28 22:29:39 +0100

    to-do

--- NodeApp/routes/plants.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-/* GET plants page. */
-router.get('/plants', (req, res) => {
-  res.render('plants');
-});
-
-module.exports = router;
