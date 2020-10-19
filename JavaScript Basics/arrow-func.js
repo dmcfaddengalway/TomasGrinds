d2a10b0588bf982ac4c0bc0f4ffbdbcf004db2bf
Daniel McFadden <dmcfaddengalway@gmail.com>
2020-10-19 22:04:09 +0100

    fourth 19/10 update

--- JavaScript Basics/arrow-func.js
+++ JavaScript Basics/arrow-func.js
@@ -3,18 +3,23 @@
 
 // Arrow functions are very simple just shorthand form of writing functions. Nothing else.
 
-// Old long way
+// Old way
 function foo(x) {
- console.log('bar ' + x);
+  console.log('bar ' + x);
+  console.log('pub ' + x);
 }
 
 // Arrow function
+//func name in variable = (arguments) => {
+  // body of function
+// }
 let baz = (x) => {
   console.log('buzz ' + x);
+  console.log('kill ' + x);
 }
 
 // Even faster use of arrow functions -- if we just perform one line, we can do this:
-biff = (x) => console.log('burr ' + x);
+let biff = (x) => console.log('burr ' + x);
 // We could drop the () for the parentheses around the 'x' [our arguments] if we only have one
 // biff = x => console.log('burr ' + x);
 
