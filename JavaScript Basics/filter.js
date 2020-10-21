66f10c5b19bb856202bd1675300b508ffa0b52eb
Daniel McFadden <dmcfaddengalway@gmail.com>
2020-10-21 19:06:36 +0100

    second 21/10 update

--- JavaScript Basics/filter.js
+++ JavaScript Basics/filter.js
@@ -4,13 +4,13 @@ function canVote(age) {
 
 function func() {
   let voterAges = [24, 33, 16, 40];
-  var filtered = voterAges.filter(canVote);
+  var filtered = voterAges.filter(voterAge => canVote(voterAge));
   console.log(filtered);
 }
 
 // func() --> voterAge = 24 --> filter() --> canVote(24) --> 24 > 18 --> 24
-// func() --> voterAge = 33 --> filter() --> canVote(33) --> 33 > 18 --> 33
-// func() --> voterAge = 16 --> filter() --> canVote(24) --> 26 < 18 --> X
-// func() --> voterAge = 40 --> filter() --> canVote(40) --> 40 > 18 --> 40
+// voterAge = 33 --> filter() --> canVote(33) --> 33 > 18 --> 33
+// voterAge = 16 --> filter() --> canVote(24) --> 26 < 18 --> X
+// voterAge = 40 --> filter() --> canVote(40) --> 40 > 18 --> 40
 
 func();
